ecc6d9db65c8af143935f8a235ae84adb554124b
Akshit Kumar <akshitkumar1999@gmail.com>
2026-05-20 15:47:24 +0530

    Connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Backend API URL
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://pageturner-bookstore.onrender.com/api";
 
 // Create axios instance
 const api = axios.create({
